b78b7875db608f435d7d09f2c3584fe8a0338d56
Eswaraprasath802 <eswaraprasath74@gmail.com>
2026-09-22 02:56:02 +0000

    Make server non-blocking and keep client blocking

--- nonblocking_client.py
+++ nonblocking_client.py
@@ -4,21 +4,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(("127.0.0.1", 5000))
 
-print("1. Connected to server")
-
-client.setblocking(False)
-
-print("2. Client socket is NON-BLOCKING")
-
-try:
-    data = client.recv(1024)
-    print("3. Received:", data.decode())
-
-except BlockingIOError:
-    print("3. No data available right now!")
+print("Client connected.")
+input("Press ENTER to send data...")
 
 client.sendall(b"Hello Server")
-
-print("4. Data sent")
 
 client.close()
